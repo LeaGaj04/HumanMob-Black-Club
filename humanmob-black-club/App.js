60fd3519cad43a27738c7a93af141e58ce243440
Matias Ignacio Briones Alegria <mati.brionesa@duocuc.cl>
2025-05-14 19:12:29 -0400

    Arreglo de problemas
    Arreglamos problemas con la version de node

--- humanmob-black-club/App.js
+++ humanmob-black-club/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import './src/services/firebase';
 import AppNavigator from './src/navigation/AppNavigator';
+import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <AuthProvider>
+      <AppNavigator />
+    </AuthProvider>
+  );
 }
--- humanmob-black-club/src/navigation/AppNavigator.js
+++ humanmob-black-club/src/navigation/AppNavigator.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+
 import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 import HomeScreen from '../screens/HomeScreen';
+
+import { AuthContext } from '../context/AuthContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    // Aquí podrías poner un splash o loader
+    return null;
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator>
+        {user ? (
+          // Usuario logueado
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          // Usuario no logueado
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
